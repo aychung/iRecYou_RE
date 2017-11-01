e8e1d75f866222a11555198891f2273dbda3b393
Albert Chung <aychung86 (at) gmail.com>
2017-11-01 14:58:11 -0700

    bus fetcher

---
+++ dbWorker/index.js
@@ -0,0 +1,42 @@
+const Consumer = require('sqs-consumer');
+const AWS = require('../util/aws.js');
+
+
+class sqsWorker {
+  // messageHanlder = (message, done) => {}
+  constructor (qUrl, messageHandler) {
+    this.worker = Consumer.create({
+      queueUrl: qUrl,
+      handleMessage: messageHandler,
+      waitTimeSeconds: 0,
+      sqs: AWS.sqs,
+    });
+    this.worker.on('error', (err) => {
+      console.log(err.message);
+    });
+  }
+
+  start () {
+    this.worker.start();
+  }
+
+  stop () {
+    this.worker.stop();
+  }
+}
+
+// const w1 = new sqsWorker('https://sqs.us-west-1.amazonaws.com/587282304975/iRecYou_RE_userAction', 
+//   (message, done) => {
+//     console.log('w1:', message.MessageId, message.Body);
+//     done();
+//   });
+// const w2 = new sqsWorker('https://sqs.us-west-1.amazonaws.com/587282304975/iRecYou_RE_searchQuery', 
+//   (message, done) => {
+//     console.log('w2:', message.MessageId, message.Body);
+//     done();
+//   });
+// w1.start();
+// w2.start();
+
+module.exports.sqsWorker = sqsWorker;
+
